00e84ad8f10fb397a2d4e82f1a9f38f2dc244d4d
Munavar Harris <60294955+mharris-t@users.noreply.github.com>
2024-01-29 14:20:10 +0200

    add governance folder and commit provider

---
+++ governance-init/providers.tf
@@ -0,0 +1,14 @@
+provider "google" {
+  project     = "my-governance-project"
+  region      = "europe-north1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-state-gov"
+    prefix  = "terraform/state"
+  }
+}
+
+#Utilize GOOGLE_APPLICATION_CREDENTIALS env variable to authenticate local machine or pipeline host
+
