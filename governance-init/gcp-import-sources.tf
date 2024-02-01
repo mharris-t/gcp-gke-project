27d3a95975e71d1510f8dbf48415ceac85a2e4f8
Munavar Harris <60294955+mharris-t@users.noreply.github.com>
2024-02-01 16:41:04 +0200

    fix gcp-projects and import-sources

--- governance-init/gcp-import-sources.tf
+++ governance-init/gcp-import-sources.tf
@@ -5,3 +5,7 @@ data "google_secret_manager_secret" "billing" {
 data "google_secret_manager_secret" "org_id" {
   secret_id = "my_org_id"
 }
+
+data "google_service_account" "gov_svc_acc" {
+  account_id = "governance" #governance@governance-234234.iam.gserviceaccount.com
+}
--- governance-init/gcp-projects.tf
+++ governance-init/gcp-projects.tf
@@ -1,3 +1,17 @@
+module "folders" {
+  source  = "terraform-google-modules/folders/google"
+  version = "~> 4.0"
+
+  parent  = "${data.google_secret_manager_secret.org_id.secret_id}"
+
+  names   = [flatten([for env_key, value in local.project_env:[env_key]])]
+
+  all_folder_admins = [
+    "serviceAccount:${data.google_service_account.gov_svc_acc.email}",
+  ]
+}
+
+
 module "project-factory" {
   source               = "terraform-google-modules/project-factory/google"
   version              = "~> 14.4"
@@ -9,7 +23,7 @@ module "project-factory" {
   usage_bucket_prefix  = "gke-project/${each.key}/1/integration"
   billing_account      = data.google_secret_manager_secret.billing.secret_id
   activate_apis        = each.value.api_list
-
+  depends_on           = [ module.folders ]
 }
 
 
