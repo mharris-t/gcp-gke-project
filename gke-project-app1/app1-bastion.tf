f25637dc0a5ce8ee573dd04f695b098b30aa37e7
Munavar Harris <60294955+mharris-t@users.noreply.github.com>
2024-02-13 23:17:32 +0200

    add app1-bastion.tf

---
+++ gke-project-app1/app1-bastion.tf
@@ -0,0 +1,51 @@
+resource "google_compute_instance" "app1_vms" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+locals {
+    app1-bastion = {
+        machine_type = "e2-micro"
+        zone         = "europe-north1-a"
+        tags         = ["app1", "bastion"]
+        image        = "ubuntu-os-cloud/ubuntu-2204-lts"
+
+    }
+}
